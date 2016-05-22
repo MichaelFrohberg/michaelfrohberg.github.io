0ddbda811e7d59ee1417fa2f2aae31a99594ff45
Michael Frohberg <michael.frohberg@gmail.com>
2016-05-21 23:41:48 -0400

    update import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { MichaelfrohbergAppComponent, environment } from './app/';
+import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(MichaelfrohbergAppComponent);
+bootstrap(AppComponent);
